9d7bad9fca65ac0a919c9bd4958968530def49eb
Charles Jacquet <charles.jacquet7@gmail.com>
2015-10-19 20:33:28 +0200

    à peut près aucune modifications, mais je dois pull

--- src/archandperfs1/task2/Task2Curves.java
+++ src/archandperfs1/task2/Task2Curves.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Scanner;
 
 import archandperfs1.BytehitrateWarmingCache;
@@ -19,7 +20,7 @@ public class Task2Curves {
 		PrintWriter prlru = new PrintWriter("result_lru_task2.txt");
 		PrintWriter prrlf = new PrintWriter("result_rlf_task2.txt");
 		
-		for (int size = 0; size <= 500000; size += 1000){
+		for (int size = 0; size <= 1; size += 1000){
 			System.out.println(size +" "+ (size < Integer.MAX_VALUE));
 			int x = size;
 			BytehitrateWarmingCache lru = new LRUCache(size, x);
@@ -27,20 +28,29 @@ public class Task2Curves {
 			BytehitrateWarmingCache rlf = new RLFCache(size, x);
 			
 			FileReader fr;
-			
+			HashMap<String, Integer> mapping = new HashMap<>();
 			try{
 			fr = new FileReader ("trace.txt"); 
+			double Count = 0;
 			Scanner in = new Scanner(fr);
 			while(in.hasNext()) {
-
-				Request res = new Request(in.next(), in.nextInt());
-				lru.process(res);
-				lfu.process(res);
-				rlf.process(res);
+				int c = 0;
+				String key = "";
+			
+				Request res = new Request(key = in.next(), c= in.nextInt());
+				
+				if (!mapping.containsKey(key)) {
+					Count += (double) c;
+					mapping.put(key , c);
+				}
+				
+//				lru.process(res);
+//				lfu.process(res);
+//				rlf.process(res);
 			}
 			in.close();
 			fr.close();
-			
+			System.out.println("haha "+Count);
 			prlru.write(String.format("%d %.1f %.1f\n", size, lru.hitRate()*100, lru.byteHitRate()*100));
 			prlru.flush();
 			prlfu.write(String.format("%d %.1f %.1f\n", size, lfu.hitRate()*100, lfu.byteHitRate()*100));
